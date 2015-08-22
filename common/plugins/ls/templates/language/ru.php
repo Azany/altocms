725245a2e5e8cddd0470317a0b98392d4940c5b7
idem_id <idem-id@yandex.ru>
2015-08-22 16:25:49 +0300

    замена слова "Топик" на " Пост"

--- common/plugins/ls/templates/language/ru.php
+++ common/plugins/ls/templates/language/ru.php
@@ -2,14 +2,14 @@
 
 return array(
     'block_create'                => 'Создать',
-    'block_create_topic_topic'    => 'Топик',
+    'block_create_topic_topic'    => 'Пост',
     'block_create_topic_question' => 'Опрос',
     'block_create_topic_link'     => 'Ссылка',
     'block_create_topic_photoset' => 'Фотосет',
     'block_create_blog'           => 'Блог',
     'block_create_talk'           => 'Сообщение',
 
-    'topic_menu_add'              => 'Топик',
+    'topic_menu_add'              => 'Пост',
     'topic_menu_add_topic'        => 'Текстовый',
     'topic_menu_add_question'     => 'Опрос',
     'topic_menu_add_link'         => 'Ссылка',
