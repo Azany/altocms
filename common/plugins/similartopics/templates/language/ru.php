7855def5ae1cf0ec104618a7694f5f9199a0289d
idem_id <idem-id@yandex.ru>
2015-08-22 15:51:44 +0300

    правка виджета similartopics (похожие посты)

--- common/plugins/similartopics/templates/language/ru.php
+++ common/plugins/similartopics/templates/language/ru.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-    'widget_title' => 'Похожие статьи',
-    'more_topics' => 'Есть еще похожие статьи'
+    'widget_title' => 'Похожие посты',
+    'more_topics' => 'Есть еще похожие посты'
 );
 
 // EOF
