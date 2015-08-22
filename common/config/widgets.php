42f627c9ceeca680d9499e682a7092527ae1dcf8
idem_id <idem-id@yandex.ru>
2015-08-22 14:18:58 +0300

    увеличиваем количество отображаемых тегов в Виджете тегов до 20

--- common/config/widgets.php
+++ common/config/widgets.php
@@ -82,7 +82,7 @@ $config['widgets']['tags'] = array(
         'tag',
     ),
     'params' => array(
-        'limit' => 70, // max items for display
+        'limit' => 20, // max items for display
     ),
 );
 
